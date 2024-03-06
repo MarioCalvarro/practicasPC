7d0a2afbb5f07b36e456ede5f32b9d23b00a2c01
Mario <mcalvarr@ucm.es>
2024-03-06 12:56:22 +0100

    3.1

---
+++ P3/src/pack/App.java
@@ -0,0 +1,33 @@
+package pack;
+
+import java.util.concurrent.Semaphore;
+
+public class App {
+
+	private static void parte1() {
+		Entero v = new Entero(0);
+		int N = 10;
+		Thread[] hilos = new Thread[2 * N];
+		Semaphore s = new Semaphore(1, true); // Fair
+		for (int i = 0; i < 2 * N; i++) {
+			boolean par = i % 2 == 0;
+			hilos[i] = new MyHilo1(i, par, v, s);
+			hilos[i].start();
+		}
+
+		for (Thread t : hilos) {
+			try {
+				t.join();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("Terminada la ejecucción, v es " + v.getValue());
+	}
+
+	public static void main(String[] args) {
+		parte1();
+	}
+
+}
---
+++ P3/src/pack/MyHilo1.java
@@ -0,0 +1,48 @@
+package pack;
+
+import java.lang.Thread;
+import java.util.concurrent.Semaphore;
+
+public class MyHilo1 extends Thread {
+	private int id;
+	private boolean par;
+	private Entero v;
+	private Semaphore s;
+
+	private static int N = 1000;
+
+	MyHilo1(int id, boolean par, Entero v, Semaphore s) {
+		this.id = id;
+		this.par = par;
+		this.v = v;
+		this.s = s;
+	}
+
+	public void run() {
+		if (par) {
+			int i = 0;
+			while (i < N) {
+				try {
+					s.acquire();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				v.incrementar();
+				s.release();
+				i += 1;
+			}
+		} else {
+			int i = 0;
+			while (i < N) {
+				try {
+					s.acquire();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				v.decrementar();
+				s.release();
+				i += 1;
+			}
+		}
+	}
+}
