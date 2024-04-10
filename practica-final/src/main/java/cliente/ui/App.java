bcbbd645c2cd5ff9fc0800ead1ad5bac50dfbeaa
Mario <mcalvarr@ucm.es>
2024-04-10 13:59:34 +0200

    Inicio cliente

--- practica-final/src/main/java/cliente/ui/App.java
+++ practica-final/src/main/java/cliente/ui/App.java
@@ -1,7 +1,12 @@
 package cliente.ui;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido cliente, introduzca su nombre");    
+        String nombre = sc.nextLine();
+        sc.close();
     }
 }
