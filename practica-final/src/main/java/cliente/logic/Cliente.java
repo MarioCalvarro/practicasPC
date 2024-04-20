4fc63beaa16153913ec8ac4404b016e137a50ea8
beñat P. de Arenaza <benperez@ucm.es>
2024-04-20 11:18:37 +0200

    Creación metodos Cliente y oyenteServidor

--- practica-final/src/main/java/cliente/logic/Cliente.java
+++ practica-final/src/main/java/cliente/logic/Cliente.java
@@ -6,22 +6,40 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.management.RuntimeErrorException;
+
+import mensaje.MsjListaUsuarios;
+import mensaje.MsjString;
+import mensaje.MsjVacio;
+import mensaje.TipoMensaje;
+import servidor.logic.Usuario;
 
 
 public class Cliente {
     private String nombre;
-    private ServerSocket ss;
+	private ObjectOutputStream fOut;
+	private boolean conexionTerminada;
+	private Usuario usuario;
+	private List<String> lista;
+
+
 
     public Cliente(String nombre) {
         this.nombre = nombre;
+        this.conexionTerminada = false;
+        this.lista = new ArrayList<String>();
+        this.usuario= new Usuario (nombre, lista);
     }
 
     public String getNombre() {
         return nombre;
     }
     
-    public void main(String args[]) throws UnknownHostException, IOException{ 	
+    public void main(String args[]) throws UnknownHostException, IOException, ClassNotFoundException{ 	
     	Scanner scanner = new Scanner(System.in);
     	System.out.println("Bienvenido. Introduzca el nombre de usuario:");
     	String name = scanner.next();
@@ -29,18 +47,67 @@ public class Cliente {
     	cliente.Start();	
     }
     
-    private void Start() throws UnknownHostException, IOException {
-    	ss = new ServerSocket(2024);
+    private void Start() throws UnknownHostException, IOException, ClassNotFoundException {
     	//TODO: Cambiar puerto
-    	 Socket cs = new Socket("localhost", 2024);
-         OyenteServidor hc = new OyenteServidor(nombre, cs);
-         hc.start();
-         try {
-             hc.join();
-         } catch (InterruptedException e) {
-             e.printStackTrace();
-         }	
+    	Socket cs = new Socket("localhost", 2024);
+        fOut = new ObjectOutputStream(cs.getOutputStream());
+        OyenteServidor hc = new OyenteServidor(nombre, cs);
+        hc.start();
+        fOut.writeObject(new MsjString(TipoMensaje.MSJ_CONEXION, usuario));
+        fOut.flush();
+        
+        while(!conexionTerminada) {
+            gestionarAcciones(pedirAcciones());
+        }
+        System.out.println("Has finalizado la conexión con el servidor.");
+        try {
+            hc.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }	
     	
+    }
+    private int pedirAcciones() { 	
+        Scanner scanner = new Scanner(System.in);
+    	System.out.println("Elige la acción que deseas hacer: ");
+        System.out.println("1.Consultar información disponible en el sistema.");
+        System.out.println("2.Descargar información deseada.");
+        System.out.println("3.Terminar sesión.");
+    	return Integer.parseInt(scanner.next());
+    }
+    
+    private void gestionarAcciones(int num) throws ClassNotFoundException, IOException {
+    	switch(num) {
+    	case 1:
+    		consultarInformacion();
+    		break;
+    	case 2:
+    		descargarInformacion();
+    		break;
+    	case 3:
+    		finalizarConexion();
+    		break;
+    	default:
+    		//TODO
+    		throw new RuntimeErrorException(null, "Numero no valido");
+    	}
+    }
+    private void consultarInformacion() throws IOException, ClassNotFoundException {
+    	fOut.writeObject(new MsjVacio(TipoMensaje. MSJ_LU));
+        fOut.flush();
+    }
+    private void descargarInformacion() throws IOException {
+    	Scanner scanner = new Scanner(System.in);
+    	System.out.println("Introduce el nombre del fichero que deseas descargar: ");
+    	String nombre = scanner.next();
+    	fOut.writeObject(new MsjString(TipoMensaje. MSJ_PEDIR_FICHERO, nombre));
+        fOut.flush();
+    	
+    }
+    private void finalizarConexion() throws IOException {
+    	fOut.writeObject(new MsjVacio(TipoMensaje. MSJ_CERRAR_CONEXION));
+        fOut.flush();
+        conexionTerminada = true;
     }
     // pedir nombre ususario
     // relizar la conexion con el servidor 
--- practica-final/src/main/java/cliente/logic/OyenteServidor.java
+++ practica-final/src/main/java/cliente/logic/OyenteServidor.java
@@ -3,11 +3,13 @@ package cliente.logic;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 import javax.management.RuntimeErrorException;
 
+import mensaje.Mensaje;
 import mensaje.MsjListaUsuarios;
 import mensaje.MsjString;
 import mensaje.MsjVacio;
@@ -19,7 +21,7 @@ public class OyenteServidor extends Thread {
 	private boolean conexionTerminada;
 	private Socket cs;
 	private String nombre;
-	private ObjectOutputStream fOut;
+    private ServerSocket ss;
 	private ObjectInputStream fIn;
 	
 	public OyenteServidor(String nombre, Socket cs){
@@ -27,7 +29,6 @@ public class OyenteServidor extends Thread {
 		this.cs = cs;
 		this.nombre = nombre;	
 		try {
-            fOut = new ObjectOutputStream(cs.getOutputStream());
             fIn = new ObjectInputStream(cs.getInputStream());
         } catch (Exception e) {
             e.printStackTrace();
@@ -35,66 +36,45 @@ public class OyenteServidor extends Thread {
 	}
     @Override
     public void run() {	
-    	try {
-			fOut.writeObject(new MsjString(TipoMensaje.MSJ_CONEXION, nombre));
-            fOut.flush();
-            MsjVacio msj = (MsjVacio) fIn.readObject();
-            System.out.println("Conectado el cliente " + nombre);
-            gestionarAcciones(pedirAcciones());
-           
-            
-            
-    	} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-    private int pedirAcciones() { 	
-        Scanner scanner = new Scanner(System.in);
-    	System.out.println("Elige la acción que deseas hacer: ");
-        System.out.println("1.Consultar información disponible en el sistema.");
-        System.out.println("2.Descargar información deseada.");
-        System.out.println("3.Terminar sesión.");
-    	return Integer.parseInt(scanner.next());
-    }
-    private void gestionarAcciones(int num) {
-    	switch(num) {
-    	case 1:
-    		consultarInformacion();
-    		break;
-    	case 2:
-    		descargarInformacion();
-    		break;
-    	case 3:
-    		finalizarConexion();
-    		break;
-    	default:
-    		//TODO
-    		throw new RuntimeErrorException(null, "Numero no valido");
-    	}
-    }
-    
-    private void consultarInformacion() throws IOException, ClassNotFoundException {
-    	fOut.writeObject(new MsjVacio(TipoMensaje. MSJ_LU));
-        fOut.flush();
-        MsjListaUsuarios msj = (MsjListaUsuarios) fIn.readObject();
-        msj.getContenido().toString();
-    }
-    private void descargarInformacion() {
+    		try {
+				ss = new ServerSocket(2025);
+	    		gestionarPeticiones();
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     	
     }
-    private void finalizarConexion() {
-    	
-    }
-    
-    
-    public void main(String args[]) {
-    	
+   
+    public void main(String args[]) {	
     	while(!conexionTerminada) {
     		this.run();
     	}    	
+    }
+    
+    private void gestionarPeticiones() throws ClassNotFoundException, IOException {
+    	Mensaje msj = (Mensaje) fIn.readObject();   	
+    	switch(msj.getTipo()) {
+    		case MSJ_CONF_CONEXION:
+    			System.out.println("Conectado el cliente " + nombre);               	
+    			break;
+    		case MSJ_CONF_LU:
+                ((MsjListaUsuarios)msj).getContenido().toString();
+                break;
+    		case MSJ_PEDIR_FICHERO:
+    			System.out.println("El fichero " + ((MsjString)msj).getContenido().toString() + " no está disponible.");
+    		case MSJ_PREPARADO_SC:
+    			break;
+    		case MSJ_INICIO_EMISION_FICHERO:
+    			break;
+    		case MSJ_FIN_EMISION_FICHERO:
+    			break;
+    		default:
+    			
+    	}
     }
 }
