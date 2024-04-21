c4d55a22d20c18dd143834256eab033734c68d8d
mario <mcalvarr@ucm.es>
2024-04-21 18:40:09 +0200

    Mensaje serializable

--- practica-final/src/main/java/mensaje/Mensaje.java
+++ practica-final/src/main/java/mensaje/Mensaje.java
@@ -1,6 +1,8 @@
 package mensaje;
 
-public abstract class Mensaje {
+import java.io.Serializable;
+
+public abstract class Mensaje implements Serializable {
     protected TipoMensaje msj;
 
     public TipoMensaje getTipo() {
